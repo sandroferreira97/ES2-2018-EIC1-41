a19d4b839c120f7eb895e27face8266deb7fdd3a
nuno <nfsfo1@iscte.pt>
2018-03-12 23:49:22 +0100

    Enviar emails de pedido de ajuda ao admnistrador

--- Baseline/src/optimizationOnDemand/Email.java
+++ Baseline/src/optimizationOnDemand/Email.java
@@ -1,8 +1,48 @@
 package optimizationOnDemand;
 
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 public class Email {
 
 	public static void enviar(String email, String assunto, String mensagem){
-		//enviar email
+		final String username = "eic2.2018.41@gmail.com";
+		final String password = "duartenunosandro";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("eic2.2018.41@gmail.com"));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("eic2.2018.41@gmail.com"));
+			message.setSubject("HELP REQUEST FROM:    "+ email + "   SUBJECT:   "+assunto);
+			message.setText(mensagem);
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
 	}
 }
