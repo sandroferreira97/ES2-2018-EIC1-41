bb803be0efda1befbcf4a0bad13d9dd21adbbe7a
dmscs-iscteiulpt <dmscs@iscte-iul.pt>
2018-03-14 20:15:43 +0000

    Adaptação do codigo para quando receber os resultados dos algoritmos

--- Baseline/src/perfil4a/DesenhoGraficos.java
+++ Baseline/src/perfil4a/DesenhoGraficos.java
@@ -1,32 +1,79 @@
 package perfil4a;
+
+
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.RenderedImage;
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
 
 public class DesenhoGraficos extends JPanel {
 	
 	private GraficosJanela gj;
+	private ArrayList <Integer> values = new ArrayList<Integer>();  
 	
-	public DesenhoGraficos (GraficosJanela gj){
+	public DesenhoGraficos (GraficosJanela gj, ArrayList values){
 		this.gj=gj;
+		this.values=values;
 	}
 	
 	public void paintComponent (Graphics g){
 		super.paintComponent(g);
-		this.setBackground(Color.WHITE);
+		
+		//Graphics2D g2d = (Graphics2D)g;
+		//g2d.rotate(Math.toRadians(90));
+		
+		//AffineTransform at = AffineTransform.getTranslateInstance(400, 400);
+		//at.rotate(Math.toRadians(90));
+		
+		this.setBackground(Color.DARK_GRAY);
+		
+		int y = 0;
+		int a = 1;
+		
+		for(int i = 0; i != values.size(); i++){
+			
+			g.setColor(Color.ORANGE);
+			g.fillRect(46, 36+y, values.get(i), 30);
+			
+			g.setColor(Color.WHITE);
+			g.drawString("g"+a, 31, 56+y);//dar nomes aos graficos
+			g.drawString(Integer.toString(values.get(i)), values.get(i)+46, 56+y);//dar valores
+			
+			y = y+34;
+			a++;
+			
+		}
 		
 		
-		g.setColor(Color.ORANGE);
-		g.fillRect(46, 36, 100, 30);
-		
-		g.setColor(Color.CYAN);
-		g.fillRect(46, 70, 75, 30);
-		
-		g.setColor(Color.BLACK);
+		g.setColor(Color.WHITE);
 		g.fillRect(45, 25, 1, gj.getHeight());
 		g.fillRect(45, 25, gj.getWidth(), 1);
-		g.drawString("g1", 31, 56);
-		g.drawString("146", 136, 15);
+		
+		
+		
+		/*
+		g2d.setColor(Color.ORANGE);
+		g2d.fillRect(46, 36, 100, 30);
+		
+		
+		g2d.setColor(Color.GREEN);
+		g2d.fillRect(46, 70, 75, 30);
+		
+		g2d.setColor(Color.BLACK);
+		g2d.fillRect(45, 25, 1, gj.getHeight());
+		g2d.fillRect(45, 25, gj.getWidth(), 1);
+		g2d.drawString("g1", 31, 56);//x-15 y+20
+		g2d.drawString("100", 146, 15);
+		g2d.drawString("g2",31 , 90);//x-15 y+20*/
+		
+		
+		
 	}
 	
 }
+
+
--- Baseline/src/perfil4a/GraficosJanela.java
+++ Baseline/src/perfil4a/GraficosJanela.java
@@ -1,15 +1,27 @@
 package perfil4a;
+
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 
 public class GraficosJanela {
 	
 	private JFrame j;
+	private ArrayList <Integer> values = new ArrayList<Integer>(); 
 	
 	public void init(){
 		
+		int a = 100;
+		int b = 140;
+		int c = 125;
+		
+		values.add(a);
+		values.add(b);
+		values.add(c);
+		
 		j = new JFrame ("Teste");
 		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		DesenhoGraficos dg = new DesenhoGraficos(this);
+		DesenhoGraficos dg = new DesenhoGraficos(this, values);
 		j.add(dg);
 		j.setSize(400, 400);
 		j.setResizable(false);
