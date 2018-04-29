76fc067bda40cb317a30b719c9d4533ac4e9de16
smsfa1-iscteiulpt <smsfa1@iscte-iul.pt>
2018-04-29 16:32:04 +0100

    Escolha dos algoritmos implementada na GUI

--- Baseline/src/optimizationOnDemand/Gui.java
+++ Baseline/src/optimizationOnDemand/Gui.java
@@ -244,10 +244,66 @@ public class Gui {
 		lblOptimizationType.setBounds(40, 350, 150, 16);
 		configadvanced.add(lblOptimizationType);
 		
+		JLabel lblAlgorithms = new JLabel("Multi-Objective Algorithms:");
+		lblAlgorithms.setBounds(40, 400, 200, 16);
+		configadvanced.add(lblAlgorithms);
+		lblAlgorithms.setVisible(false);
+		
+		JComboBox MoAlgorithms = new JComboBox();
+		MoAlgorithms.setModel(new DefaultComboBoxModel(new String[] {"", "AbYSS", "CellDE", "dMPOSO", "GDE3", "FastPGA", "IBEA", "MOCHC", "MOCell", "MOEA/D-DE", "pMOEA/D-DE", "MOEA/D-DRA", "NSGA-II", "ssNSGA-II", "NSGAIIr", "NSGAIIa", "pNSGA-II", "OMOPSO", "PAES", "SMPSO", "pSMPSO", "SMPSOhv", "SPEA2"}));
+		MoAlgorithms.setBounds(255, 400, 116, 22);
+		configadvanced.add(MoAlgorithms);
+		MoAlgorithms.setVisible(false);
+		
+		JLabel lblAlgorithms2 = new JLabel("Single-Objective Algorithms:");
+		lblAlgorithms2.setBounds(40, 430, 200, 16);
+		configadvanced.add(lblAlgorithms2);
+		lblAlgorithms2.setVisible(false);
+		
+		JComboBox SoAlgorithms = new JComboBox();
+		SoAlgorithms.setModel(new DefaultComboBoxModel(new String[] {"", "Genetic Algorithms", "PSO", "Differential Evolution", "Evolution Strategy", "CMA-ES" }));
+		SoAlgorithms.setBounds(255, 430, 116, 22);
+		configadvanced.add(SoAlgorithms);
+		SoAlgorithms.setVisible(false);
+
+		
 		JComboBox OptimizationType = new JComboBox();
 		OptimizationType.setModel(new DefaultComboBoxModel(new String[] {"", "Manual", "Automatic", "Mixed"}));
 		OptimizationType.setBounds(255, 350, 116, 22);
 		configadvanced.add(OptimizationType);
+		
+		OptimizationType.addActionListener(new ActionListener(){
+	        public void actionPerformed(ActionEvent e){
+	        	
+	        	if(OptimizationType.getSelectedItem().toString().equals("Manual")){
+	        		lblAlgorithms.setVisible(true);
+	        		MoAlgorithms.setVisible(true);
+	        		lblAlgorithms2.setVisible(true);
+	        		SoAlgorithms.setVisible(true);
+	        	}
+	        	
+	        	if(OptimizationType.getSelectedItem().toString().equals("")){
+	        		lblAlgorithms.setVisible(false);
+	        		MoAlgorithms.setVisible(false);
+	        		lblAlgorithms2.setVisible(false);
+	        		SoAlgorithms.setVisible(false);
+	        	}
+	        	
+	        	if(OptimizationType.getSelectedItem().toString().equals("Automatic")){
+	        		lblAlgorithms.setVisible(false);
+	        		MoAlgorithms.setVisible(false);
+	        		lblAlgorithms2.setVisible(false);
+	        		SoAlgorithms.setVisible(false);
+	        	}
+	        	
+	        	if(OptimizationType.getSelectedItem().toString().equals("Mixed")){
+	        		lblAlgorithms.setVisible(false);
+	        		MoAlgorithms.setVisible(false);
+	        		lblAlgorithms2.setVisible(false);
+	        		SoAlgorithms.setVisible(false);
+	        	}
+	        }
+		});
 			
 		
 		JButton btnGenerate = new JButton("Generate");
@@ -267,7 +323,7 @@ public class Gui {
 			}
 		});
 		
-		btnGenerate.setBounds(255, 474, 97, 25);
+		btnGenerate.setBounds(255, 500, 97, 25);
 		configadvanced.add(btnGenerate);
 		
 		
