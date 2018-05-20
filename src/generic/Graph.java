d7246c92f7c20d8d516a01fcc14c8671605d847a
dmscs-iscteiulpt <dmscs@iscte-iul.pt>
2018-05-20 16:45:30 +0100

    String no eixo do X

--- src/generic/Graph.java
+++ src/generic/Graph.java
@@ -39,8 +39,8 @@ public class Graph extends ApplicationFrame {
    
    private JPanel createChartPanel() { // this method will create the chart panel containin the graph 
 	    String chartTitle = "Objects Movement Chart";
-	    String xAxisLabel = "X";
-	    String yAxisLabel = "Y";
+	    String xAxisLabel = "              FN                           FP                                                                                                            ";
+	    String yAxisLabel = "Valores";
 	    
 	    XYDataset dataset = createDataset();
 	    
@@ -96,28 +96,28 @@ public class Graph extends ApplicationFrame {
 	    XYSeries series3 = new XYSeries("Object 3");
 	    
 	    
-	    
 	    series1.add(1.0, 2.0);
 	    series1.add(2.0, 3.0);
 	    series1.add(3.0, 2.5);
-	    series1.add(3.5, 2.8);
-	    series1.add(4.2, 6.0);
+	    series1.add(4.0, 2.8);
+	    series1.add(5.0, 6.0);
 	    
-	    series2.add(2.0, 1.0);
-	    series2.add(2.5, 2.4);
-	    series2.add(3.2, 1.2);
-	    series2.add(3.9, 2.8);
-	    series2.add(4.6, 3.0);
+	    series2.add(1.0, 1.0);
+	    series2.add(2.0, 2.4);
+	    series2.add(3.0, 1.2);
+	    series2.add(4.0, 2.8);
+	    series2.add(5.0, 3.0);
 	    
-	    series3.add(1.2, 4.0);
-	    series3.add(2.5, 4.4);
-	    series3.add(3.8, 4.2);
-	    series3.add(4.3, 3.8);
-	    series3.add(4.5, 4.0);
+	    series3.add(1.0, 4.0);
+	    series3.add(2.0, 4.4);
+	    series3.add(3.0, 4.2);
+	    series3.add(4.0, 3.8);
+	    series3.add(5.0, 4.0);
 	    
 	    dataset.addSeries(series1);
 	    dataset.addSeries(series2);
 	    dataset.addSeries(series3);
+	    
 	    
 	    return dataset;
    }  
